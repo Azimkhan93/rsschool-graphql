2113ac0975acc90b7c34f78ee8b84118592fee10
Azimkhan93 <aabdulsatarov@gmail.com>
2024-03-03 23:11:13 +0500

    fix: nourish post, profile and user types

--- src/routes/graphql/types/profileType.ts
+++ src/routes/graphql/types/profileType.ts
@@ -1,28 +1,35 @@
-import { GraphQLBoolean, GraphQLInt, GraphQLList, GraphQLNonNull, GraphQLObjectType } from "graphql";
-import { MemberTypeId as MemberTypeIdFromSchema } from "../../member-types/schemas.js";
-import { MemberTypeId } from "./memberType.js";
-import { UUIDType } from "./uuid.js";
-
-
-export type Profile = {
-  id: string;
-  isMale: boolean;
-  yearOfBirth: number;
-  userId: string;
-  memberTypeId: MemberTypeIdFromSchema | string;
-};
-
-const ProfileFields = {
-  id: {type: new GraphQLNonNull(UUIDType)},
-  isMale: {type: GraphQLBoolean},
-  yearOfBirth: {type: GraphQLInt},
-  userId: {type: UUIDType},
-  memberTypeId: {type: MemberTypeId },
-};
+import {
+  GraphQLBoolean,
+  GraphQLInt,
+  GraphQLList,
+  GraphQLNonNull,
+  GraphQLObjectType,
+} from 'graphql';
+import { Profile as ProfileTypeFromPrisma } from '@prisma/client';
+import { MemberType, MemberTypeId } from './memberType.js';
+import { UUIDType } from './uuid.js';
+import { UserType } from './userType.js';
+import prismaClient from './context.js';
 
 export const ProfileType = new GraphQLObjectType({
   name: 'ProfileType',
-  fields: ProfileFields,
+  fields: () => ({
+    id: { type: new GraphQLNonNull(UUIDType) },
+    isMale: { type: GraphQLBoolean },
+    yearOfBirth: { type: GraphQLInt },
+    userId: { type: new GraphQLNonNull(UUIDType) },
+    memberTypeId: { type: new GraphQLNonNull(MemberTypeId) },
+    user: {
+      type: UserType,
+      resolve: async ({ userId }: ProfileTypeFromPrisma, __: unknown) =>
+        await prismaClient.user.findUnique({ where: { id: userId } }),
+    },
+    memberType: {
+      type: MemberType as GraphQLObjectType,
+      resolve: async ({ memberTypeId }: ProfileTypeFromPrisma, __: unknown) =>
+        await prismaClient.memberType.findUnique({ where: { id: memberTypeId } }),
+    },
+  }),
 });
 
 export const ProfilesType = new GraphQLList(ProfileType);
--- src/routes/graphql/types/userType.ts
+++ src/routes/graphql/types/userType.ts
@@ -1,21 +1,57 @@
-import { GraphQLFloat, GraphQLObjectType, GraphQLString } from "graphql";
-import { UUIDType } from "./uuid.js";
-
-export type Post = {
-  id: string;
-  name: string;
-  balance: number;
-};
-
-
-const UserFields = {
-  id: { type: UUIDType },
-  name: { type: GraphQLString },
-  balance: {type: GraphQLFloat},
-}
-
+import {
+  GraphQLFloat,
+  GraphQLList,
+  GraphQLNonNull,
+  GraphQLObjectType,
+  GraphQLString,
+} from 'graphql';
+import { UUIDType } from './uuid.js';
+import { User as UserTypeFromPrisma } from '@prisma/client';
+import prismaClient from './context.js';
+import { ProfileType } from './profileType.js';
+import { PostsType } from './postType.js';
 
 export const UserType = new GraphQLObjectType({
   name: 'UserType',
-  fields: UserFields,
+  fields: () => ({
+    id: { type: new GraphQLNonNull(UUIDType) },
+    name: { type: GraphQLString },
+    balance: { type: GraphQLFloat },
+    profile: {
+      type: ProfileType as GraphQLObjectType,
+      resolve: async ({ id }: UserTypeFromPrisma) =>
+        await prismaClient.profile.findUnique({ where: { userId: id } }),
+    },
+    posts: {
+      type: PostsType,
+      resolve: async ({ id }: UserTypeFromPrisma) =>
+        await prismaClient.post.findMany({ where: { authorId: id } }),
+    },
+    userSubscribedTo: {
+      type: new GraphQLList(UserType),
+      resolve: async ({ id }: UserTypeFromPrisma) =>
+        await prismaClient.user.findMany({
+          where: {
+            subscribedToUser: {
+              some: {
+                subscriberId: id,
+              },
+            },
+          },
+        }),
+    },
+    subscribedToUser: {
+      type: new GraphQLList(UserType),
+      resolve: async ({ id }: UserTypeFromPrisma) =>
+        await prismaClient.user.findMany({
+          where: {
+            userSubscribedTo: {
+              some: {
+                authorId: id,
+              },
+            },
+          },
+        }),
+    },
+  }),
 });
